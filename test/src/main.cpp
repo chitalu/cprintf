12d6d19f6667132830e53914d266a8795bbc7e82
floyd_chitalu <Floyd.Chitalu@null.net>
2014-08-23 23:43:36 +0100

    fixed cmake file to build samples with new dir structure. added basic google test project. Creates new cmake file to deal with the creation of gtest project

---
+++ test/src/main.cpp
@@ -0,0 +1,14 @@
+#include <cprintf/cprintf.h>
+#include <gtest/gtest.h>
+
+TEST(Stub, foo)
+{
+	ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	int ret = RUN_ALL_TESTS();
+	return ret;
+}
