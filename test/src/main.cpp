a8c014d3e669a428bde98a881ca58a273da4292a
floyd_chitalu <Floyd.Chitalu@null.net>
2014-08-28 22:46:17 +0100

    refactored test structures

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,43 +3,42 @@
 
 TEST(BadToken, using_bitmap_fg_tok_on_windows)
 {
-	ASSERT_THROW(	cprintf("quux $64f xterm bitmap colour is an error\n"), 
+	ASSERT_THROW(	cprintf("cpf test :: $64f xterm bitmap colour is an error\n"), 
 					_cpf_type::error);
-	cprintf("$!");
+	printf("\n");
 }
 
 TEST(BadToken, using_bitmap_bg_tok_on_windows)
 {
-	ASSERT_THROW(	cprintf("quux $64b xterm bitmap colour is an error\n"),
+	ASSERT_THROW(	cprintf("cpf test :: $64b xterm bitmap colour is an error\n"),
 					_cpf_type::error);
-	cprintf("$!");
+	printf("\n");
 }
 
 TEST(BadToken, using_bitmap_fgbg_tok_on_windows)
 {
-	ASSERT_THROW(	cprintf("quux $128f.16b xterm bitmap colour is an error\n"),
+	ASSERT_THROW(	cprintf("cpf test :: $128f.16b xterm bitmap colour is an error\n"),
 					_cpf_type::error);
-	cprintf("$!");
+	printf("\n");
 }
 
 TEST(BadToken, using_bitmap_ampersand_fgbg_tok_on_windows)
 {
-	ASSERT_THROW(	cprintf("quux $64& xterm bitmap colour is an error\n"),
+	ASSERT_THROW(	cprintf("cpf test :: $64& xterm bitmap colour is an error\n"),
 					_cpf_type::error);
-	cprintf("$!");
+	printf("\n");
 }
 
-TEST(Arg, printing_stl_string)
+TEST(Arg, printing_std_string)
 {
 	std::string s = "l love cprintf";
-	ASSERT_NO_THROW(cprintf("print std::string %s", s));
-	cprintf("$!");
+	ASSERT_NO_THROW(cprintf("cpf test :: print std::string -->> %s\n", s));
 }
 
 int main(int argc, char **argv)
 {
-	cprintf("foo $r*g* bar!!");
 	::testing::InitGoogleTest(&argc, argv);
 	int ret = RUN_ALL_TESTS();
+	std::getchar();
 	return ret;
 }
