c93cf7de245d255db222415a5f00e4d86f8bdf90
chitalu <floyd.m.chitalu@gmail.com>
2017-06-03 00:46:38 +0100

    add python examples

--- cprintf.py
+++ cprintf.py
@@ -7,10 +7,6 @@ import types
 import collections
 import fnmatch
 
-_module_abs_path = os.path.abspath(__file__)
-_module_real_path = os.path.realpath(_module_abs_path)
-_module_dir_name = os.path.dirname(_module_real_path)
-
 _CPF_STDO = 0x1
 _CPF_STDE = 0X2
 
@@ -23,10 +19,7 @@ _MAX_ARGS_COUNT = 2
 # path to the shared library with exported cprintf symbols 
 # client scripts may change this variable to indicate a different directory 
 # The default search path is within the script's directory
-lib_path = os.path.join(_module_dir_name, "libcprintf.so")
-cpf_base_path = os.path.join(_module_dir_name, "../../incl/cprintf/internal/cpf_base.h")
-
-_lib_status_codes = None
+lib_path = None
 
 # names for some object types that are used by the standard 
 # Python interpreter, but not for the types defined by 
@@ -47,53 +40,25 @@ def _ctype_get(var):
     elif isinstance(var, types.UnicodeType): return ctypes.c_wchar_p
     elif isinstance(var, file): return ctypes.c_uint8
     
-    raise TypeError("CPF-RT-ERR: Invalid Type -->", type(var).__name__)
+    raise TypeError("cprintf error: Invalid Type -->", type(var).__name__)
 
 def _promote_args(*args):
     return tuple(_ctype_get(a)(a) for a in args) 
-
-def _infer_API_name(args):
-    # first two arguments have predetermined types
-    name = "_cprintf_{0}"
-    # remove the "c_" part of type name
-    stripped_ctype_names = [type(a).__name__[2:] for a in args]
-    # identify type by first letter
-    type_char0s = map(lambda e: e[0], stripped_ctype_names)
-    # append to produce API name
-    return name.format("".join(type_char0s))
     
 def invoke(lib_hndl, *args):
     # convert user args to their ctypes equivalents
     promoted_form = _promote_args(*args)
     # deduce API name to call based on argument types
-    API_name = _infer_API_name(promoted_form)
+    API_name = "cprintf_{0}".format(promoted_form)
+
     if not hasattr(lib_hndl, API_name):
-        type_combo = "--> " + ", ".join([type(a).__name__ for a in args])
-        raise ValueError("CPF-RT-ERR: Unsupported Type combination:\n{0}".format(type_combo))
+        raise ValueError("cprintf error: Unsupported Type combination")
 
     # get specific API handle
     lib_func = getattr(lib_hndl, API_name)
     lib_func.restype = ctypes.c_int
     lib_func.argtypes = [type(arg) for arg in promoted_form]
     return lib_func(*promoted_form)
-
-def _scan_status_codes():
-    global _lib_status_codes
-
-    global _lib
-    global lib_path
-    _lib_status_codes = dict()
-    with open(cpf_base_path, "r") as cpf_base_file:
-        lines = cpf_base_file.readlines()
-        for line in lines:
-            if fnmatch.fnmatch(line, "*CPF_*_ERR*"):
-                spos = line.rfind(' ') + 1
-                radix = 16
-                code = int(line[spos : ], radix)
-                idx = lines.index(line)
-                diag_msg = lines[idx - 1]
-
-                _lib_status_codes[code] = diag_msg[2:].strip()
 
 # handle to the shared library
 _lib = None
@@ -102,21 +67,17 @@ def _init_once():
     global _lib
     global lib_path
 
-    if _lib is not None and _lib_status_codes is not None:
+    if _lib is not None:
         return
     
     if (_lib is None):
+        if not os.path.exists(lib_path):
+            raise RuntimeError("cprintf error: library path does not exist")
         _lib = ctypes.CDLL(lib_path, mode=ctypes.RTLD_GLOBAL)
 
-    if _lib_status_codes is None:
-        _scan_status_codes()
-
-def _validate(status):
-    if ( status not in _lib_status_codes):
-        raise RuntimeError("CPF-RT-ERR: Unknown {0}".format(status))
-    
+def _validate(status):    
     if status != 0:
-        raise RuntimeError("CPF-RT-ERR: {0}".format(_lib_status_codes[status]))
+        raise RuntimeError("cprintf error: {0}".format(status))
 
 def cprintf(*VA_ARGS):
     """
@@ -130,25 +91,23 @@ def cprintf(*VA_ARGS):
     global _lib
 
     status = None
-   
     arg_cnt = len(VA_ARGS)
     if not (arg_cnt >= _MIN_ARGS_COUNT and arg_cnt <= _MAX_ARGS_COUNT):
-        raise ValueError("CPF-RT-ERR: Invalid argument count: {0}".format(arg_cnt))
+        raise ValueError("cprintf error: Invalid argument count: {0}".format(arg_cnt))
  
     is_str = lambda e: isinstance(e, types.StringType) or isinstance(e, types.UnicodeType) 
     arg0 = VA_ARGS[0]
 
     # emulate standard "print" function
-    if not (isinstance(arg0, file) and ((arg0 is sys.stdout) or (arg0 is sys.stderr))) and \
+    if not (isinstance(arg0, type(sys.stdout)) and ((arg0 is sys.stdout) or (arg0 is sys.stderr))) and \
         not is_str(arg0):
         print(*VA_ARGS)
         return
            
     stream = None
-
     # the first argument after the format string
     fmtspec_arg = None
-    if isinstance(arg0, file):
+    if isinstance(arg0, type(sys.stdout)):
         stream = _CPF_STDO if (arg0 is sys.stdout) else _CPF_STDE
         if arg_cnt > 2:
             fmtspec_arg = VA_ARGS[2]
@@ -160,13 +119,13 @@ def cprintf(*VA_ARGS):
     
     if not (fmtspec_arg is None) and is_iterable(fmtspec_arg) and is_list_or_tup(fmtspec_arg):
         if any([is_iterable(x) for x in fmtspec_arg]):
-            raise ValueError("CPF-RT-ERR: Invalid expandable argument: {0}".format(fmtspec_arg))
+            raise ValueError("cprintf error: Invalid expandable argument: {0}".format(fmtspec_arg))
         a_ = None
         if not (stream is None):
             a_ = (VA_ARGS[_STREAM], VA_ARGS[_FORMAT]) + tuple(fmtspec_arg)
         else:
             a_ = (arg0, ) + fmtspec_arg
-        status = cprintf(*a_)
+        status = invoke(_lib, *a_)
     else:
         forwarded_args = None
         if stream is None:
@@ -179,6 +138,3 @@ def cprintf(*VA_ARGS):
 
 if __name__ == '__main__':
     pass
-    # TODO
-    # import doctest
-    # doctest.testmod()
--- cprintf_examples.py
+++ cprintf_examples.py
@@ -1,103 +1,150 @@
+import os
+import sys
+
+_module_abs_path = os.path.abspath(__file__)
+_module_real_path = os.path.realpath(_module_abs_path)
+_module_dir_name = os.path.dirname(_module_real_path)
+
+# location of cprintf script
+sys.path.insert(0, _module_dir_name)
+
 from cprintf import cprintf
 from sys import stdout, stderr
 
-def basic_samples(): 
-    # NOTE: The following emulates the behaviour of the standard python 
-    # "print()" function. Nothing new
-    cprintf(75)
-    cprintf(5.9)
-    cprintf(["this is NOT a format string %d\n", 73, "foo"], 0.2)
-    cprintf({1 : "foo", 2.4 : 2, "bar" : 75})
-    cprintf({1 : "foo", 2.4 : 2, "bar" : 75}, "cprintf")
+def main():
+    cprintf(stdout, "Characters: $R%c %c \n", 'a', 65)
+    #cprintf(stderr, "$GDecimals$?: %d %ld\n", 1977, 650000L)
+    cprintf("$BPreceding with blanks: %10d \n", 1977)
+    cprintf(L"Preceding with zeros: $Y%010d \n", 1977)
+    #cprintf("Some different radices: %d %x %o $M%#x$W %#o \n", 100, 100, 100, 100, 100)
+	#cprintf("floats: %4.2f $C%+.0e$? %E \n", 3.1416, 3.1416, 3.1416)
+	cprintf("$W%s \n", "A string")
+	cprintf("$r%-5s\n", "ab")
+	cprintf("String: $cR%s\n", "my string arg")
+	cprintf("Str$Mi$?ngs:\n")
+	const char *s = "Hello"
+	cprintf("Char$cacters:\t%c %%\n", 65)
+	#cprintf("\t$Y.%10s$?.\n\t.%-10s.", s, s)
+	cprintf("$YIntegers\n")
+	#cprintf("Decimal:\t$R%i $G%d $B%.6i $C%i $Y%.0i $M%+i $W%u\n", 1, 2, 3, 0, 0, 4, -1)
+	#cprintf("Hexadecimal:\t$R#%x $G#%x $B#%X $C#%#x\n", 5, 10, 10, 6)
+	#cprintf("$b#Octal:\t%o %#o %#o\n", 10, 10, 4)
+	cprintf("$cFloating point\n")
+	#cprintf("Ro$yunding:\t%f $M%.0f $W%.32f\n", 1.5, 1.5, 1.3)
+	#cprintf("$WPadding:\t%05.2f %.2f %5.2f\n", 1.5, 1.5, 1.5)
+	#cprintf("Scientific:$R\t%E $W%e\n", 1.5, 1.5)
+	#cprintf("$BHexadecimal:\t%a %A\n", 1.5, 1.5)
+	std::uint32_t val = std::numeric_limits<std::uint32_t>::max()
+	cprintf("Largest 32-bit value is $Y%" PRIu32 "$? or $C%#" PRIx32 "\n", val, val)
+    # --
+    # Unsupported 
+	#cprintf("\t.%10s.\n\t.%-10s.\n\t.%*s.\n", s, s, 10, s)
+	#cprintf("Special values:\t0/0=%g 1/0=%g\n", 0. / 0, 1. / 0)
+	#cprintf("right-justified variable width: '%*c'\n", 5, 'x')
+    # --
+	cprintf("std::string: %s\n", std::string("hello"))
+	cprintf("std::string: %S\n", std::wstring(L"hello"))
+	# cprintf_s("string literal API\n")
+    # --
+    # Unsupported
+	# cprintf_s(s)
+    # --
+	cprintf("bar $rw`Red on white\n")
+	cprintf("bar $r#ed\n")
+	cprintf("bar $r#ed\n")
+	cprintf("bar $r`#ed\n")
+	cprintf("quux$r.b blue\n")
+	cprintf("quux$B.r red\n")
+	cprintf("quux$B.R red\n")
+	cprintf("quux$r.B blue\n")
+#ifdef CPF_WINDOWS_BUILD
+	#cprintf("note that $bld this text is blue\n")
+#else
+	# bitmap colour tokens available on XTERM (0 - 255)
+	cprintf("quux $128f xterm bitmap colour foreground\n")
+	cprintf("quux $24b xterm bitmap colour background\n")
+	cprintf("quux $128f.94b xterm bitmap colour foreground & background\n")
+	cprintf("quux $128f$64b xterm bitmap colour foreground & background\n")
+	# bitmap colour foreground and background
+	cprintf("quux $64& xterm bitmap colour block\n")
+	cprintf("note that $bld this text is bold\n")
+	cprintf("note that $b`ld this text is blue\n")
+	# reverting text format token effects mid format string
+	cprintf("drive forward and $rvs reverse $?rvsand the back to normal\n")
+#endif
+	 xplat_tokens = (
+		"r", "g", "b", "y", "m", "c", "w",
+		"r#", "g#", "b#", "y#", "m#", "c#", "w#",
+		"R", "G", "B", "Y", "M", "C", "W",
+		"R#", "G#", "B#", "Y#", "M#", "C#", "W#",
+		"rr", "rb", "rg", "ry", "rm", "rc", "rw",
+		"gg", "gb", "gr", "gy", "gm", "gc", "gw",
+		"bb", "br", "bg", "by", "bm", "bc", "bw",
+		"yy", "yb", "yg", "yr", "ym", "yc", "yw",
+		"mm", "mr", "mg", "my", "mb", "mc", "mw",
+		"cc", "cr", "cg", "cy", "cm", "cb", "cw",
+		"ww", "wr", "wg", "wy", "wm", "wc", "wb",
+		"Rr", "Rb", "Rg", "Ry", "Rm", "Rc", "Rw",
+		"Gg", "Gb", "Gr", "Gy", "Gm", "Gc", "Gw",
+		"Bb", "Br", "Bg", "By", "Bm", "Bc", "Bw",
+		"Yy", "Yb", "Yg", "Yr", "Ym", "Yc", "Yw",
+		"Mm", "Mr", "Mg", "My", "Mb", "Mc", "Mw",
+		"Cc", "Cr", "Cg", "Cy", "Cm", "Cb", "Cw",
+		"Ww", "Wr", "Wg", "Wy", "Wm", "Wc", "Wb",
+		"rR", "rB", "rG", "rY", "rM", "rC", "rW",
+		"gG", "gB", "gR", "gY", "gM", "gC", "gW",
+		"bB", "bR", "bG", "bY", "bM", "bC", "bW",
+		"yY", "yB", "yG", "yR", "yM", "yC", "yW",
+		"mM", "mR", "mG", "mY", "mB", "mC", "mW",
+		"cC", "cR", "cG", "cY", "cM", "cB", "cW",
+		"wW", "wR", "wG", "wY", "wM", "wC", "wB",
+		"RR", "RB", "RG", "RY", "RM", "RC", "RW",
+		"GG", "GB", "GR", "GY", "GM", "GC", "GW",
+		"BB", "BR", "BG", "BY", "BM", "BC", "BW",
+		"YY", "YB", "YG", "YR", "YM", "YC", "YW",
+		"MM", "MR", "MG", "MY", "MB", "MC", "MW",
+		"CC", "CR", "CG", "CY", "CM", "CB", "CW",
+		"WW", "WR", "WG", "WY", "WM", "WC", "WB"
+	)
 
-    # NOTE: This is the "cprintf" way of doing things!
-    cprintf("this IS a format string\n") 
-    cprintf(stdout, "$y*hello world!\n")
-    cprintf("print pi: %f\n", 3.14)
-    cprintf(stderr, "write to $r*stderr$?`!\n")
-    cprintf(stderr, "$m*`formatted$? arg: %d!\n", 101)
-    binary = 0xB01101001
-    cprintf("print binary: %b\n", binary)
-    my_tup = (107, 3.142)
-    cprintf(stderr, "$m*`formatted$? args: $g*%d$? %f!\n", my_tup)
-    my_list = ["cprintf", 99, 2.5]
-    cprintf(stderr, "$w*`formatted$? args: $c%s %d %f!\n", my_list)
-   
-def display_tokens():
-    token_types = (bg, ibg, fg, ifg, fgbg, ifgbg, fgibg, ifgibg) = tuple(range(0, 8))
-    tokens = ("r", "g", "b", "y", "m", "c", "w")
-    l = list()
-    for ct in token_types:
-        for t in tokens:
-            if ct == bg:
-                l.append( t+"#")
-            elif ct == ibg:
-                l.append(t+"*#")
-            elif ct == fg:
-                l.append(t)
-            elif ct == ifg:
-                l.append(t+"*")
-            elif ct == fgbg:
-                for t_ in tokens:
-                    l.append(t+t_)
-            elif ct == ifgbg:
-                for t_ in tokens:
-                    l.append(t+"*"+t_)
-            elif ct == fgibg:
-                for t_ in tokens:
-                    l.append(t + t_+"*")       
-            elif ct == ifgibg:
-                for t_ in tokens:
-                    l.append(t+"*"+t_+"*")
+	for (tokenIter in xplat_tokens):
+		format = ("$" + tokenIter + "%s" + "$?\t")
+		cprintf(format, tokenIter)
+		i = xplat_tokens.index(tokenIter) + 1
+		if ((i % 7) == 0):
+			cprintf("%s", "\n\n" if i == (xplat_tokens.size() -1) else "\n")
 
-    cprintf(stderr, "\nCROSS-PLATFORM COLOURS\n")
-    for token in l:
-        frmt = "$" + token + " " + str(l.index(token)) + "$?\t"
-        cprintf(stderr, frmt) 
-        if (l.index(token) % 16) == 1:    
-            cprintf(stderr, "\n")
-    cprintf(stderr, "\n")
+	cprintf("`$? `$r `$g `$b `$y `$m `$c `$w `$rg `$gB `$By `$YM `$m ...\n")
+	cprintf("$? $r`r $g`g $b`b $y`y $m`m $c`c $w`w $rg`rg $gB`gB $By`By $YM`YM $m`m ...\n")
 
-    cprintf(stderr, "\n---LINUX-SPECIFIC COLOURS---\n")
+    cprintf("\n---LINUX-SPECIFIC COLOURS---\n")
 
-    cprintf(stderr, "\nBITMAP FOREGROUND\n")
+    cprintf("\nBITMAP FOREGROUND\n")
     for i in range(0, 256):
         frmt =  "$" + str(i) + "f " + str(i) + "$?\t"
-        cprintf(stderr, frmt)
+        cprintf(frmt)
         if (i % 16)+1 == 1:
-            cprintf(stderr, "\n")
-    cprintf(stderr, "\n")
+            cprintf("\n")
+    cprintf( "\n")
 
 
-    cprintf(stderr, "\nBITMAP BACKGROUND\n")
+    cprintf("\nBITMAP BACKGROUND\n")
     for i in range(0, 256):
         frmt =  "$" + str(i) + "b " + str(i) + "$?\t"
-        cprintf(stderr, frmt)
+        cprintf(frmt)
         if (i % 16)+1 == 1:
-            cprintf(stderr, "\n")
-    cprintf(stderr, "\n")
+            cprintf("\n")
+    cprintf("\n")
 
-    cprintf(stderr, "\nBITMAP FOREGROUND & BACKGROUND\n")
+    cprintf("\nBITMAP FOREGROUND & BACKGROUND\n")
     for i in range(0, 256):
         frmt =  "$" + str(i) + "& " + str(i) + "$?\t"
-        cprintf(stderr, frmt)
+        cprintf(frmt)
         if (i % 16)+1 == 1:
-            cprintf(stderr, "\n")
-    cprintf(stderr, "\n")
-
-def main():
-    basic_samples()
-    display_tokens()
+            cprintf("\n")
+    cprintf("\n")
 
 if __name__ == '__main__':
-    import os
-    _module_abs_path = os.path.abspath(__file__)
-    _module_real_path = os.path.realpath(_module_abs_path)
-    _module_dir_name = os.path.dirname(_module_real_path)
-
-    # location of cprintf script
-    sys.path.insert(0, os.path.join(_module_dir_name, "./interface"))
-
     # notify cprintf module that you want to use this lib
     # by default the script expects to find the libcprintf.so/cprintf.dll
     # file in the same directory as itself
