1065168dc0c2d0b1ef2a1aa11d4110758e5e67e4
chitalu <floyd.m.chitalu@gmail.com>
2017-06-03 01:03:38 +0100

    reformat2

--- cprintf_examples.py
+++ cprintf_examples.py
@@ -22,7 +22,7 @@ def main():
 	cprintf("$r%-5s\n", "ab")
 	cprintf("String: $cR%s\n", "my string arg")
 	cprintf("Str$Mi$?ngs:\n")
-	const char *s = "Hello"
+	s = "Hello"
 	cprintf("Char$cacters:\t%c %%\n", 65)
 	#cprintf("\t$Y.%10s$?.\n\t.%-10s.", s, s)
 	cprintf("$YIntegers\n")
@@ -34,8 +34,8 @@ def main():
 	#cprintf("$WPadding:\t%05.2f %.2f %5.2f\n", 1.5, 1.5, 1.5)
 	#cprintf("Scientific:$R\t%E $W%e\n", 1.5, 1.5)
 	#cprintf("$BHexadecimal:\t%a %A\n", 1.5, 1.5)
-	std::uint32_t val = std::numeric_limits<std::uint32_t>::max()
-	cprintf("Largest 32-bit value is $Y%" PRIu32 "$? or $C%#" PRIx32 "\n", val, val)
+	#std::uint32_t val = std::numeric_limits<std::uint32_t>::max()
+	#cprintf("Largest 32-bit value is $Y%" PRIu32 "$? or $C%#" PRIx32 "\n", val, val)
     # --
     # Unsupported 
 	#cprintf("\t.%10s.\n\t.%-10s.\n\t.%*s.\n", s, s, 10, s)
@@ -73,38 +73,25 @@ def main():
 	cprintf("drive forward and $rvs reverse $?rvsand the back to normal\n")
 #endif
 	 xplat_tokens = (
-		"r", "g", "b", "y", "m", "c", "w",
-		"r#", "g#", "b#", "y#", "m#", "c#", "w#",
-		"R", "G", "B", "Y", "M", "C", "W",
-		"R#", "G#", "B#", "Y#", "M#", "C#", "W#",
-		"rr", "rb", "rg", "ry", "rm", "rc", "rw",
-		"gg", "gb", "gr", "gy", "gm", "gc", "gw",
-		"bb", "br", "bg", "by", "bm", "bc", "bw",
-		"yy", "yb", "yg", "yr", "ym", "yc", "yw",
-		"mm", "mr", "mg", "my", "mb", "mc", "mw",
-		"cc", "cr", "cg", "cy", "cm", "cb", "cw",
-		"ww", "wr", "wg", "wy", "wm", "wc", "wb",
-		"Rr", "Rb", "Rg", "Ry", "Rm", "Rc", "Rw",
-		"Gg", "Gb", "Gr", "Gy", "Gm", "Gc", "Gw",
-		"Bb", "Br", "Bg", "By", "Bm", "Bc", "Bw",
-		"Yy", "Yb", "Yg", "Yr", "Ym", "Yc", "Yw",
-		"Mm", "Mr", "Mg", "My", "Mb", "Mc", "Mw",
-		"Cc", "Cr", "Cg", "Cy", "Cm", "Cb", "Cw",
-		"Ww", "Wr", "Wg", "Wy", "Wm", "Wc", "Wb",
-		"rR", "rB", "rG", "rY", "rM", "rC", "rW",
-		"gG", "gB", "gR", "gY", "gM", "gC", "gW",
-		"bB", "bR", "bG", "bY", "bM", "bC", "bW",
-		"yY", "yB", "yG", "yR", "yM", "yC", "yW",
-		"mM", "mR", "mG", "mY", "mB", "mC", "mW",
-		"cC", "cR", "cG", "cY", "cM", "cB", "cW",
-		"wW", "wR", "wG", "wY", "wM", "wC", "wB",
-		"RR", "RB", "RG", "RY", "RM", "RC", "RW",
-		"GG", "GB", "GR", "GY", "GM", "GC", "GW",
-		"BB", "BR", "BG", "BY", "BM", "BC", "BW",
-		"YY", "YB", "YG", "YR", "YM", "YC", "YW",
-		"MM", "MR", "MG", "MY", "MB", "MC", "MW",
-		"CC", "CR", "CG", "CY", "CM", "CB", "CW",
-		"WW", "WR", "WG", "WY", "WM", "WC", "WB"
+        "r",  "g",  "b",  "y",  "m",  "c",  "w",  "r#", "g#", "b#", "y#", "m#",
+        "c#", "w#", "R",  "G",  "B",  "Y",  "M",  "C",  "W",  "R#", "G#", "B#",
+        "Y#", "M#", "C#", "W#", "rr", "rb", "rg", "ry", "rm", "rc", "rw", "gg",
+        "gb", "gr", "gy", "gm", "gc", "gw", "bb", "br", "bg", "by", "bm", "bc",
+        "bw", "yy", "yb", "yg", "yr", "ym", "yc", "yw", "mm", "mr", "mg", "my",
+        "mb", "mc", "mw", "cc", "cr", "cg", "cy", "cm", "cb", "cw", "ww", "wr",
+        "wg", "wy", "wm", "wc", "wb", "Rr", "Rb", "Rg", "Ry", "Rm", "Rc", "Rw",
+        "Gg", "Gb", "Gr", "Gy", "Gm", "Gc", "Gw", "Bb", "Br", "Bg", "By", "Bm",
+        "Bc", "Bw", "Yy", "Yb", "Yg", "Yr", "Ym", "Yc", "Yw", "Mm", "Mr", "Mg",
+        "My", "Mb", "Mc", "Mw", "Cc", "Cr", "Cg", "Cy", "Cm", "Cb", "Cw", "Ww",
+        "Wr", "Wg", "Wy", "Wm", "Wc", "Wb", "rR", "rB", "rG", "rY", "rM", "rC",
+        "rW", "gG", "gB", "gR", "gY", "gM", "gC", "gW", "bB", "bR", "bG", "bY",
+        "bM", "bC", "bW", "yY", "yB", "yG", "yR", "yM", "yC", "yW", "mM", "mR",
+        "mG", "mY", "mB", "mC", "mW", "cC", "cR", "cG", "cY", "cM", "cB", "cW",
+        "wW", "wR", "wG", "wY", "wM", "wC", "wB", "RR", "RB", "RG", "RY", "RM",
+        "RC", "RW", "GG", "GB", "GR", "GY", "GM", "GC", "GW", "BB", "BR", "BG",
+        "BY", "BM", "BC", "BW", "YY", "YB", "YG", "YR", "YM", "YC", "YW", "MM",
+        "MR", "MG", "MY", "MB", "MC", "MW", "CC", "CR", "CG", "CY", "CM", "CB",
+        "CW", "WW", "WR", "WG", "WY", "WM", "WC", "WB"
 	)
 
 	for (tokenIter in xplat_tokens):
